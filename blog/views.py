d472362773d08171dad6e657e552959de8e9b4b3
arezoo darvishi <arezoo.darvish6969@gmail.com>
2024-04-07 11:33:53 +0000

    Finish html frameworks

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.utils import timezone
+from blog.models import Post
 
-# Create your views here.
+def index(request):
+  return render(request, "blog/index.html")
